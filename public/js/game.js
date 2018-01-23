4feb3d9fbb930c21bceec6c6a686f80e4a9cfd3b
Bryce Sharp <Bryce@BryceSharp.com>
2018-01-23 04:26:21 -0600

    Auto stash before merge of "master" and "Graphs"

--- public/js/game.js
+++ public/js/game.js
@@ -708,6 +708,526 @@ if (false) {
   }
 }
 
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-0bb45336", Component.options)
+  } else {
+    hotAPI.reload("data-v-0bb45336", Component.options)
+  }
+  module.hot.dispose(function (data) {
+    disposed = true
+  })
+})()}
+
+module.exports = Component.exports
+
+
+/***/ }),
+
+/***/ 135:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+var _methods;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+    props: ['segments', 'instance', 'gamer'],
+    data: function data() {
+        return {
+            range: _.range(0, this.segments * this.segments, 1),
+            size: 300,
+            grid: 100,
+            offset: 0,
+            localInstance: this.instance
+        };
+    },
+    methods: (_methods = {
+        rectClicked: function rectClicked(id) {
+            GameEvent.fire('locationSelected', id);
+        },
+        setSize: function setSize() {
+            var w = window,
+                d = document,
+                e = d.documentElement,
+                g = d.getElementsByTagName('body')[0],
+                x = w.innerWidth || e.clientWidth || g.clientWidth,
+                y = w.innerHeight || e.clientHeight || g.clientHeight;
+            this.size = (x < y ? x : y) - 125;
+            this.offset = (x - this.size) / 2;
+            this.grid = this.size / this.segments;
+            document.getElementsByClassName('the-grid')[0].style.margin = "0 " + this.offset + 'px';
+        },
+        instanceWin: function instanceWin(message) {
+            alert(message.message);
+        },
+        instanceUpdated: function instanceUpdated(instance) {
+            this.currentInstance.edges = JSON.parse(instance.edges);
+        }
+    }, _defineProperty(_methods, 'instanceUpdated', function instanceUpdated(instance) {
+        console.log('instanceUpdated');
+        console.log(this.localInstance);
+        this.localInstance.edges = instance.edges;
+        console.log(this.localInstance);
+    }), _defineProperty(_methods, 'hasPlayerEdge', function hasPlayerEdge(vert) {
+        var edge = this.hasEdge(0, vert);
+        if (edge != false) {
+            return true;
+        }
+        edge = this.hasEdge(1, vert);
+        if (edge != false) {
+            return true;
+        }
+        return false;
+    }), _defineProperty(_methods, 'addEdge', function addEdge(vert1, vert2) {
+        this.instance.edges.unshift([vert1, vert2]);
+    }), _defineProperty(_methods, 'hasEdge', function hasEdge(vert1, vert2) {
+        var result = false;
+        var edges = this.instance.edges;
+        _.forEach(edges, function (edge) {
+            if (edge[0] == vert1 && edge[1] == vert2 || edge[0] == vert2 && edge[1] == vert1) {
+                result = edge;
+                return;
+            }
+        });
+        return result;
+    }), _defineProperty(_methods, 'locationSelected', function locationSelected(location) {
+        if (this.instance.id == 0) {
+            if (!this.hasPlayerEdge(location)) {
+                this.addEdge(0, location);
+                var randomStrat = 2;
+                if (this.instance.edges.length < 9) {
+                    do {
+                        randomStrat = Math.floor(Math.random() * 8) + 2;
+                    } while (this.hasPlayerEdge(randomStrat));
+                    var vm = this;
+                    setTimeout(function () {
+                        vm.addEdge(1, randomStrat);
+                    }, 100);
+                }
+            }
+        } else {
+            axios.post('api/instance/' + this.instance.id, { action: 'add', edge: [this.gamer.id, location] }).then(function (response) {});
+        }
+    }), _methods),
+    computed: {
+        playerXName: function playerXName() {
+            return this.localInstance.users[0].id == 'gamer' ? this.gamer.name : this.localInstance.users[0].name;
+        },
+        playerOName: function playerOName() {
+            return this.localInstance.users[1].id == 'gamer' ? this.gamer.name : this.localInstance.users[1].name;
+        },
+        playerXPrimary: function playerXPrimary() {
+            return this.localInstance.users[0].id == 'gamer' ? this.gamer.primaryColor : this.localInstance.users[0].primaryColor;
+        },
+        playerXSecondary: function playerXSecondary() {
+            return this.localInstance.users[0].id == 'gamer' ? this.gamer.secondaryColor : this.localInstance.users[0].secondaryColor;
+        },
+        playerOPrimary: function playerOPrimary() {
+            return this.localInstance.users[1].id == 'gamer' ? this.gamer.primaryColor : this.localInstance.users[1].primaryColor;
+        },
+        playerOSecondary: function playerOSecondary() {
+            return this.localInstance.users[1].id == 'gamer' ? this.gamer.secondaryColor : this.localInstance.users[1].secondaryColor;
+        },
+        playerExEdges: function playerExEdges() {
+            return _.filter(this.localInstance.edges, function (edge) {
+                return edge[0] == 0;
+            });
+        },
+        playerCircleEdges: function playerCircleEdges() {
+            return _.filter(this.localInstance.edges, function (edge) {
+                return edge[0] == 1;
+            });
+        }
+    },
+    mounted: function mounted() {
+        this.setSize();
+        window.addEventListener('resize', this.setSize);
+    },
+    created: function created() {
+        var _this = this;
+
+        GameEvent.listen('locationSelected', function (location) {
+            return _this.locationSelected(location);
+        });
+        GameEvent.listen('instanceUpdated', function (instance) {
+            return _this.instanceUpdated(instance);
+        });
+        GameEvent.listen('instanceWin', function (message) {
+            return _this.instanceWin(message);
+        });
+    },
+    beforeDestroy: function beforeDestroy() {
+        window.removeEventListener('resize', this.setSize);
+    }
+});
+
+/***/ }),
+
+/***/ 136:
+/***/ (function(module, exports, __webpack_require__) {
+
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "svg",
+    {
+      staticClass: "the-grid",
+      attrs: { height: _vm.size + 85, width: _vm.size }
+    },
+    [
+      _vm._l(_vm.range, function(i) {
+        return _c("rect", {
+          attrs: {
+            id: i,
+            x: (i % _vm.segments) * _vm.grid,
+            y: Math.floor(i / _vm.segments) * _vm.grid,
+            width: _vm.grid,
+            height: _vm.grid,
+            fill: "#fff"
+          },
+          on: {
+            click: function($event) {
+              _vm.rectClicked(i + 2)
+            }
+          }
+        })
+      }),
+      _vm._v(" "),
+      _vm._l(_vm.segments - 1, function(l1) {
+        return _c("line", {
+          staticClass: "the-grid-line",
+          attrs: {
+            x1: _vm.grid * l1 - 2,
+            y1: "0",
+            x2: _vm.grid * l1 - 2,
+            y2: _vm.size - 2,
+            "stroke-width": "5",
+            stroke: "black"
+          }
+        })
+      }),
+      _vm._v(" "),
+      _vm._l(_vm.segments - 1, function(l2) {
+        return _c("line", {
+          staticClass: "the-grid-line",
+          attrs: {
+            x1: "0",
+            y1: _vm.grid * l2 - 2,
+            x2: _vm.size - 2,
+            y2: _vm.grid * l2 - 2,
+            "stroke-width": "5",
+            stroke: "black"
+          }
+        })
+      }),
+      _vm._v(" "),
+      _vm._l(_vm.playerCircleEdges, function(edge) {
+        return _c("circle", {
+          attrs: {
+            cx: ((edge[1] - 2) % _vm.segments) * _vm.grid + _vm.grid / 2,
+            cy:
+              Math.floor((edge[1] - 2) / _vm.segments) * _vm.grid +
+              _vm.grid / 2,
+            r: _vm.grid / 2 - 3,
+            stroke: _vm.playerOPrimary,
+            "stroke-width": "5",
+            "fill-opacity": "0"
+          }
+        })
+      }),
+      _vm._v(" "),
+      _vm._l(_vm.playerExEdges, function(edge) {
+        return _c("line", {
+          attrs: {
+            x1: ((edge[1] - 2) % _vm.segments) * _vm.grid + 3,
+            y1: Math.floor((edge[1] - 2) / _vm.segments) * _vm.grid + 3,
+            x2: ((edge[1] - 2) % _vm.segments) * _vm.grid + _vm.grid - 3,
+            y2:
+              Math.floor((edge[1] - 2) / _vm.segments) * _vm.grid +
+              _vm.grid -
+              5,
+            "stroke-width": "5",
+            stroke: _vm.playerXPrimary
+          }
+        })
+      }),
+      _vm._v(" "),
+      _vm._l(_vm.playerExEdges, function(edge) {
+        return _c("line", {
+          attrs: {
+            x1: ((edge[1] - 2) % _vm.segments) * _vm.grid + _vm.grid - 3,
+            y1: Math.floor((edge[1] - 2) / _vm.segments) * _vm.grid + 3,
+            x2: ((edge[1] - 2) % _vm.segments) * _vm.grid + 3,
+            y2:
+              Math.floor((edge[1] - 2) / _vm.segments) * _vm.grid +
+              _vm.grid -
+              3,
+            "stroke-width": "5",
+            stroke: _vm.playerXPrimary
+          }
+        })
+      }),
+      _vm._v(" "),
+      _c(
+        "rect",
+        {
+          attrs: {
+            x: 5,
+            y: _vm.size + 5,
+            width: _vm.grid * 1.25 - 10,
+            height: 50,
+            fill: _vm.playerXPrimary,
+            "stroke-width": "5",
+            stroke: _vm.playerXSecondary,
+            rx: "4",
+            ry: "4"
+          }
+        },
+        [
+          _c("animate", {
+            staticClass: "board-load",
+            attrs: {
+              attributeName: "y",
+              dur: ".75s",
+              from: 0,
+              to: _vm.size + 5
+            }
+          })
+        ]
+      ),
+      _vm._v(" "),
+      _c(
+        "rect",
+        {
+          attrs: {
+            x: 8,
+            y: _vm.size + 8,
+            width: _vm.grid * 1.25 - 16,
+            height: 44,
+            fill: "none",
+            "stroke-width": "3",
+            stroke: "rgba(0,0,0,.5)",
+            rx: "4",
+            ry: "4"
+          }
+        },
+        [
+          _c("animate", {
+            staticClass: "board-load",
+            attrs: {
+              attributeName: "y",
+              dur: ".75s",
+              from: 0,
+              to: _vm.size + 8
+            }
+          })
+        ]
+      ),
+      _vm._v(" "),
+      _c(
+        "text",
+        {
+          staticStyle: {
+            stroke: "#000",
+            "font-size": "24px",
+            "font-weight": "700",
+            fill: "#fff"
+          },
+          attrs: { x: 5 + 10, y: _vm.size + 40 }
+        },
+        [
+          _vm._v(_vm._s(_vm.playerXName) + "\n                  "),
+          _c("animate", {
+            staticClass: "board-load",
+            attrs: {
+              attributeName: "y",
+              dur: ".75s",
+              from: 0,
+              to: _vm.size + 40
+            }
+          })
+        ]
+      ),
+      _vm._v(" "),
+      _c(
+        "text",
+        {
+          staticStyle: {
+            stroke: "#000",
+            "font-size": "32px",
+            "font-weight": "700",
+            fill: "#fff"
+          },
+          attrs: { x: 5 + _vm.grid * 1.4, y: _vm.size + 40 }
+        },
+        [_vm._v("VS\n                      ")]
+      ),
+      _vm._v(" "),
+      _c(
+        "rect",
+        {
+          attrs: {
+            x: 5 + _vm.grid * 1.75,
+            y: _vm.size + 5,
+            width: _vm.grid * 1.25 - 10,
+            height: 50,
+            fill: _vm.playerOPrimary,
+            "stroke-width": "5",
+            stroke: _vm.playerOSecondary,
+            rx: "4",
+            ry: "4"
+          }
+        },
+        [
+          _c("animate", {
+            staticClass: "board-load",
+            attrs: {
+              attributeName: "y",
+              dur: ".75s",
+              from: 0,
+              to: _vm.size + 5
+            }
+          })
+        ]
+      ),
+      _vm._v(" "),
+      _c(
+        "rect",
+        {
+          attrs: {
+            x: 8 + _vm.grid * 1.75,
+            y: _vm.size + 8,
+            width: _vm.grid * 1.25 - 16,
+            height: 44,
+            fill: "none",
+            "stroke-width": "3",
+            stroke: "rgba(0,0,0,.5)",
+            rx: "4",
+            ry: "4"
+          }
+        },
+        [
+          _c("animate", {
+            staticClass: "board-load",
+            attrs: {
+              attributeName: "y",
+              dur: ".75s",
+              from: 0,
+              to: _vm.size + 8
+            }
+          })
+        ]
+      ),
+      _vm._v(" "),
+      _c(
+        "text",
+        {
+          staticStyle: {
+            stroke: "#000",
+            "font-size": "24px",
+            "font-weight": "700",
+            fill: "#fff"
+          },
+          attrs: { x: 5 + _vm.grid * 1.75 + 10, y: _vm.size + 40 }
+        },
+        [
+          _vm._v(_vm._s(_vm.playerOName) + "\n                  "),
+          _c("animate", {
+            staticClass: "board-load",
+            attrs: {
+              attributeName: "y",
+              dur: ".75s",
+              from: 0,
+              to: _vm.size + 40
+            }
+          })
+        ]
+      )
+    ],
+    2
+  )
+}
+var staticRenderFns = []
+render._withStripped = true
+module.exports = { render: render, staticRenderFns: staticRenderFns }
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+    require("vue-hot-reload-api")      .rerender("data-v-0bb45336", module.exports)
+  }
+}
+
 /***/ }),
 
 /***/ 137:
