af47a68fb96fe520d35458570667eb56459ab7dc
Bryce Sharp <bryce@brycesharp.com>
2018-01-23 09:30:44 -0600

    Added win message to general chat

--- app/Events/InstanceWin.php
+++ app/Events/InstanceWin.php
@@ -9,6 +9,8 @@ use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Events\MessageSent;
+use PhpParser\Node\Expr\Cast\Object_;
 
 class InstanceWin  implements ShouldBroadcast
 {
@@ -25,10 +27,17 @@ class InstanceWin  implements ShouldBroadcast
     {
         $winner->points = $winner->points + 1000;
         $winner->save();
-        $this->message = "The winner is " . $winner->name . "!";
+        $this->message = "# $instance->id | The winner is " . $winner->name . "!";
         $this->instance = $instance;
         $instance->status = "Complete";
         $instance->save();
+
+        $game = new stdClass();
+        $game->name = "G.A.M.E.";
+        $game->primaryColor = "#fff";
+        $game->secondaryColor = "#fff";
+
+        event(new MessageSent($this->message, $game));
     }
 
     /**
